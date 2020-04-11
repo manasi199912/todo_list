4759773e04361a6fe5f85140143e0d9fa3849c1a
manasi199912 <53189281+manasi199912@users.noreply.github.com>
2020-04-11 20:18:20 +0530

    Create Constant.kt

---
+++ Constant.kt
@@ -0,0 +1,13 @@
+package com.example.dashboard
+
+const val DB_name="TodoList"
+const val DB_version=1
+const val Table_name="ToDo"
+const val Col_id="id"
+const val Col_Created_At="craetedAt"
+const val Col_name="name"
+
+const val Table_Todo_it="TodoItem"
+const val Col_todo_id="TodoId"
+const val Col_item_name="itemName"
+const val Col_is_completed="isCompleted"
